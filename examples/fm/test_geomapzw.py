80f43bd02a3b7cf611a5411f84b187ad45a23bfd
faraigit <faraimax@gmail.com>
2021-07-08 00:54:15 +0200

    update 8 July

---
+++ examples/fm/test_geomapzw.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `geomapzw` package."""
+
+
+import unittest
+
+from geomapzw import geomapzw
+
+
+class TestGeomapzw(unittest.TestCase):
+    """Tests for `geomapzw` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
